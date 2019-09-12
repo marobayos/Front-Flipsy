e3eefa96ee09ba9b1fd298066ef82c78576d5aeb
baguevaram <baguevaram@unal.edu.co>
2019-09-12 14:17:57 -0500

    Pantalla Registro

---
+++ src/componentes/Registro.js
@@ -0,0 +1,140 @@
+import React, {Component} from 'react';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+import { TextField } from '@material-ui/core';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import regis from './regis.svg';
+
+
+let theme = createMuiTheme(
+    {overrides: {
+            MuiInputLabel: { // Name of the component ⚛️ / style sheet
+                root: {
+                    color: "white",
+                    "&$focused": { // increase the specificity for the pseudo class
+                        color: "white"
+                    }
+                }
+            },
+            MuiInput: { // Name of the component ⚛️ / style sheet
+                root: {
+                    color: "white",
+                    "&$focused": { // increase the specificity for the pseudo class
+                        color: "white"
+                    }
+                },
+                underline:{
+                    borderBottomColor:"white"
+                }
+            },
+            MuiTypography:{
+                body1: {
+                    color: "white",
+                    "&$focused": { // increase the specificity for the pseudo class
+                        color: "white"
+                    }
+                }
+            }
+        }
+    });
+
+class Registro extends Component{
+    constructor(){
+        super();
+        this.state = {
+            correo:'',
+            usuario:'',
+            contra:'',
+            terminos: false
+        };
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChecked = this.handleChecked.bind(this);
+    }
+
+    handleInput(e){
+        const {value,name} = e.target;
+        this.setState({
+            [name]:value
+        })
+    }
+
+    handleSubmit(e){
+        
+    }
+
+    handleChecked(e) {
+        this.setState({
+            terminos: e.target.checked
+        })
+    };
+
+
+    render (){
+        return(
+            <div>
+                <img src={regis} className="img-regis" alt="logo" />
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormControl>
+                        <ThemeProvider theme={theme}>
+                            <FormGroup>
+                                <TextField
+                                    label='CORREO ELECTRÓNICO'
+                                    onChange ={this.handleInput}
+                                    name = 'correo'
+                                    type='isEmail'
+                                    required= {true}
+                                    margin= 'dense'
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <TextField
+                                    label = 'USUARIO'
+                                    onChange ={this.handleInput}
+                                    name = 'usuario'
+                                    margin= 'dense'
+                                    required= {true}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <TextField
+                                    label = 'CONTRASEÑA'
+                                    onChange ={this.handleInput}
+                                    name = 'contra'
+                                    required= {true}
+                                    margin= 'dense'
+                                    type = 'password'
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <FormControlLabel
+                                    control={
+                                        <Checkbox
+                                            checked={this.state.terminos}
+                                            onChange={this.handleChecked}
+                                            color= {"primary"}
+                                            margin= 'dense'
+                                            required= {true}
+                                        />
+                                    }
+                                    label="He leído y acepto los Términos y Condiciones"
+                                    color='white'
+                                />
+                            </FormGroup>
+                            <Button type='submit' color="primary">
+                                REGISTRARSE
+                            </Button>
+                        </ThemeProvider>
+                    </FormControl>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Registro;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache } from 'apollo-cache-inmemory';
+import App from './App';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+const httpLink = {
+    uri: 'https://api.github.com/graphql'
+};
+
+const client = new ApolloClient({
+    link: new HttpLink(httpLink),
+    cache: new InMemoryCache()
+});
+
+const Root = () => (
+ <ApolloProvider client={client}>
+    <App/>
+ </ApolloProvider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
