2d40a2f780e189c9d65871abc0db814abb735b58
jodorganistaca <jodorganistaca@unal.edu.co>
2019-09-13 22:21:43 -0500

    Updated version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
-import Presentacion from "./componentes/Presentacion";
-=======
->>>>>>> 7cb968ecb9b0653e9cb1d0af41468812676ed6c8
 import Registro from "./componentes/Registro";
+import MainLogin from "./componentes/MainLogin";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-        < Presentacion />
-=======
-        <Registro/>
->>>>>>> 7cb968ecb9b0653e9cb1d0af41468812676ed6c8
+        <MainLogin/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
-<<<<<<< HEAD
-import {ApolloClient} from 'apollo-client';
-import {HttpLink} from 'apollo-link-http';
-import {InMemoryCache } from 'apollo-cache-inmemory';
-import App from './App';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-=======
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
@@ -16,7 +8,6 @@ import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'antd/dist/antd.css'
->>>>>>> 7cb968ecb9b0653e9cb1d0af41468812676ed6c8
 
 const httpLink = {
     uri: 'https://api.github.com/graphql'
@@ -28,15 +19,9 @@ const client = new ApolloClient({
 });
 
 const Root = () => (
-<<<<<<< HEAD
- <ApolloProvider client={client}>
-    <App/>
- </ApolloProvider>
-=======
     <ApolloProvider client={client}>
         <App />
     </ApolloProvider>
->>>>>>> 7cb968ecb9b0653e9cb1d0af41468812676ed6c8
 );
 
 ReactDOM.render(<Root />, document.getElementById('root'));
