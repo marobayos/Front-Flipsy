f043e94e6856e226d49d9e6bcc163969a833c957
baguevaram <baguevaram@unal.edu.co>
2019-09-13 19:20:20 -0500

    por fin

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Registro from "./componentes/Registro";
+
+function App() {
+  return (
+    <div className="App">
+        <Registro/>
+    </div>
+  );
+}
+
+export default App;
