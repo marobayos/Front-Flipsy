ff23ec818b56f2d349404f64bdd8db5403ea3759
jodorganistaca <jodorganistaca@unal.edu.co>
2019-09-20 19:35:24 -0500

    :package: merge mcrunny

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import Login from './componentes/MainLogin'
 import Registro from './componentes/Registro'
 import CodeConfirmation from './componentes/CodConf'
 import Home from './componentes/Home'
-
-
+import Verification from './componentes/Verification'
+import Confirmation from './componentes/Confirmation'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Registro} exact />
+        <Route path="/" component={Login} exact />
         <Route path="/signup" component={Registro} />
         <Route path='/signin' component={Login} />
         <Route path='/confirm-code' component={CodeConfirmation} />
         <Route path='/home' component={Home} />
+        <Route path='/verification' component={Verification} />
+        <Route path='/confirm' component={Confirmation} />
       </Switch>
+        
     </BrowserRouter>
   );
 }
